f80f5cddfab8601a97e9e30a4019a4334c73ab0d
Phantom <37140087+Phantom115@users.noreply.github.com>
2024-05-06 14:51:29 -0400

    Create ncScan_v1.1.sh

---
+++ ncScan_v1.1.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+#This script was developed by Phantom115
+#SET THE STARTING OCTET HERE
+net="192.168.10"
+echo "Scanning $net network, enter last octet start: "
+read nStart
+echo "Enter last octet end: "
+read nEnd
+
+echo "scanning $net.$nStart - $nEnd"
+pStart="1"
+pEnd="1000"
+
+#for NUMBER in RANGE 
+
+for ((x=$nStart; $x<=$nEnd; x++))
+do
+    echo "Scanning $net.$x"
+    for ((i=$pStart; $i<=$pEnd; i++))
+    do
+        timeout 0.01 nc -nvz $net.$x $i 2>&1 | grep -E 'succ|open'
+    done
+done
