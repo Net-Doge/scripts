fc22f8f6fef53fb09607fd60a2b0fd389c34f46a
Phantom <37140087+Phantom115@users.noreply.github.com>
2024-05-06 17:34:11 -0400

    Update and rename pingSweep.sh to pingSweep_v1.1.sh

--- pingSweep_v1.1.sh
+++ pingSweep_v1.1.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+#created by Phantom115
 echo "         ▄              ▄"
 echo "        ▌▒█           ▄▀▒▌"
 echo "        ▌▒▒█        ▄▀▒▒▒▐"
@@ -19,6 +20,17 @@ echo "    ▀▄▒▒▒▒▒▒▒▒▒▒▄▄▄▀▒▒▒▒▄▀"
 echo "      ▀▄▄▄▄▄▄▀▀▀▒▒▒▒▒▄▄▀"
 echo "         ▒▒▒▒▒▒▒▒▒▒▀▀"
 
-echo "Enter First 3 octets for the ping sweep: "
+echo "Enter First 2 octets for the ping sweep: "
 read net
-for i in {1..254} ;do (ping -c 1 $net.$i | grep "bytes from" &) ;done
+echo "Enter the start of the range for the 3rd octet: "
+read start
+echo "Enter the end of the range for the 3rd octet: "
+read end
+for i in $(seq $start $end)
+do
+    echo "Scanning $net.$i.XXX"
+    for x in {1..254}
+    do
+        timeout 0.05 ping -c 1 $net.$i.$x 2>&1 | grep "bytes from" 2>&1 
+    done
+done
