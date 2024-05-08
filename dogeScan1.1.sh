e8d7ded4faa32e47cb94a6927c156881c141997b
Phantom <37140087+Phantom115@users.noreply.github.com>
2024-05-08 12:09:51 -0400

    Update and rename dogeScan1.0.sh to dogeScan1.1.sh

--- dogeScan1.1.sh
+++ dogeScan1.1.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-#created by Phantom115
+#Created by Phantom115
 echo "         ▄              ▄"
 echo "        ▌▒█           ▄▀▒▌"
 echo "        ▌▒▒█        ▄▀▒▒▒▐"
@@ -33,19 +33,20 @@ do
     echo "Scanning $net.$i.XXX"
     for x in {1..254}
     do
-        timeout 0.05 ping -c 1 $net.$i.$x 2>&1 | grep "bytes from" 2>&1 
-        (timeout 0.05 ping -c 1 $net.$i.$x 2>&1 | grep "bytes from" 2>&1) | grep -E -o '([0-9]{1,3}\.){3}[0-9]{1,3}' >> pings.txt
+        timeout 0.01 ping -c 1 $net.$i.$x 2>&1 | grep "bytes from" 2>&1 
+        (timeout 0.01 ping -c 1 $net.$i.$x 2>&1 | grep "bytes from" 2>&1) | grep -E -o '([0-9]{1,3}\.){3}[0-9]{1,3}' >> pings.txt
     done
 done
+echo "pings.txt Updated!"
 
 pStart="1"
 pEnd="1000"
 while IFS= read -r line; do
-    echo "scanning $line"
+    echo "Scanning $line Ports $pStart-$pEnd"
     for ((i=$pStart; $i<=$pEnd; i++))
     do
-        (timeout 0.01 nc -nvz $line $i 2>&1 | grep -E 'succ|open') | awk '{ print "ADDRESS: " $2 " PORT:" $3 " SERVICE:" $4 " STATUS: " $5 }' 
-        (timeout 0.01 nc -nvz $line $i 2>&1 | grep -E 'succ|open') | awk '{ print "ADDRESS: " $2 " PORT:" $3 " SERVICE:" $4 " STATUS: " $5 }' >> openPorts.txt
+        (timeout 0.01 nc -nvzw1 $line $i 2>&1 | grep -E 'succ|open') | awk '{ print "ADDRESS: " $2 " PORT: " $3 " SERVICE:" $4 " STATUS: " $5 }' 
+        (timeout 0.01 nc -nvzw1 $line $i 2>&1 | grep -E 'succ|open') | awk '{ print "ADDRESS: " $2 " PORT: " $3 " SERVICE:" $4 " STATUS: " $5 }' >> openPorts.txt
     done
 done < "pings.txt"
-echo "Contents added to openPorts.txt"
+echo "Contents Added to openPorts.txt"
