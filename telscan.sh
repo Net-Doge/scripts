fa4125e6d519486a1ac57492153343d6460624f3
net.doge <37140087+Net-Doge@users.noreply.github.com>
2025-04-23 13:13:07 -0400

    Create telscan.sh
    a simple telnet scanner

---
+++ telscan.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+echo "Filename of ips: "
+read file
+
+ips=($(awk '{print $1}' $file))
+ports=(22 53 88 135 139 389 445 464 593 636 3288 3269 3389 5985 9389)
+
+for ip in "${ips[@]}"; do 
+  echo "Scanning $ip"
+  for port in "${ports[@]}"; do
+    echo quit | timeout 0.20 telnet $ip $port 2>/dev/null | grep -q "Connected" && echo "$ip:$port - OPEN" || echo "$ip:$port - CLOSED"
+  done
+echo ""
+echo ""
+echo ""
+done
