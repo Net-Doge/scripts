9a7b25accb6f2ae464e2457cf89ba788f88034a2
net.doge <37140087+Net-Doge@users.noreply.github.com>
2025-04-23 13:39:15 -0400

    Update telscan.sh

--- telscan.sh
+++ telscan.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+ls -la
 echo "Filename of ips: "
 read file
 
@@ -8,7 +9,7 @@ ports=(22 53 88 135 139 389 445 464 593 636 3288 3269 3389 5985 9389)
 for ip in "${ips[@]}"; do 
   echo "Scanning $ip"
   for port in "${ports[@]}"; do
-    echo quit | timeout 0.20 telnet $ip $port 2>/dev/null | grep -q "Connected" && echo "$ip:$port - OPEN" || echo "$ip:$port - CLOSED"
+    echo quit | timeout 0.20 telnet $ip $port 2>/dev/null | grep -q "Connected" && echo -e "$ip:$port - \033[32mOPEN\033[0m" || echo -e "$ip:$port - \033[31mCLOSED\033[0m"
   done
 echo ""
 echo ""
